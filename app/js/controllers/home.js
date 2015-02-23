b2f22796dfe7ef6da296ec3436700d9d282fd19c
jordanh <jordanh@sfu.ca>
2015-02-23 14:20:15 -0800

    testing

--- app/js/controllers/home.js
+++ app/js/controllers/home.js
@@ -97,6 +97,48 @@ APP.controller('HomeCtrl', function ($scope) {
 		console.log(APP.akSelected);
 	}
 
+	// tile flip
+
+	$scope.akSelected = APP.akSelected;
+
+	$scope.flipped = false;
+
+	var countF = 0;
+	var countB = 0;
+
+	
+	$scope.flip = function() {
+		$scope.flipped = !$scope.flipped;
+		console.log('flipping');
+	}
+
+});
+
+APP.directive("flipper", function() {
+	return {
+		restrict: "E",
+		template: "<div class='flipper' ng-transclude ng-class='{ flipped: flipped }'></div>",
+		transclude: true,
+		scope: {
+			flipped: "="
+		}
+	};
+});
+
+APP.directive("front", function() {
+	return {
+		restrict: "E",
+		template: "<div class='front tile' ng-transclude></div>",
+		transclude: true
+	};
+});
+
+APP.directive("back", function() {
+	return {
+		restrict: "E",
+		template: "<div class='back tile' ng-transclude></div>",
+		transclude: true
+	}
 });
 
 
